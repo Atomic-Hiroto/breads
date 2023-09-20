d688d56048c654f58b82527bf282e7f906ef23ce
Pritam <borolepritam@gmail.com>
2023-09-20 15:55:16 +0530

    Added posts router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser();
+  const {data} = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -15,6 +15,13 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {user.isSignedIn ? <SignOutButton /> : <SignInButton />}
+        <div>
+          {
+            data?.map((post)=>{
+              return <div key={post.id} >{post.content}</div>
+            })
+          }
+        </div>
       </main>
     </>
   );
