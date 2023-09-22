3451be16381f02ec133893b0919ed8961a4fcaad
Pritam <borolepritam@gmail.com>
2023-09-22 18:06:34 +0530

    Fixed typescript issues

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,6 @@
 import { clerkClient } from "@clerk/nextjs";
-import { User } from "@clerk/nextjs/dist/types/server";
+import type { User } from "@clerk/nextjs/dist/types/server";
 import { TRPCError } from "@trpc/server";
-import { prependListener } from "process";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
@@ -28,7 +27,7 @@ export const postsRouter = createTRPCRouter({
     return posts.map((post)=>{
       const author = users.find((user) => user.id === post.authorId);
 
-      if(!author || !author.username) throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message:"Author for post not found"})
+      if(!author?.username) throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message:"Author for post not found"})
       
       return {
         post,
