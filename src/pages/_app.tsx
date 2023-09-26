07899fc643104b46f504d2555638ce0eae922cb2
Pritam <borolepritam@gmail.com>
2023-09-26 18:00:07 +0530

    Added profile page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,23 @@
 import { type AppType } from "next/app";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <ClerkProvider><Toaster position="bottom-center" /><Component {...pageProps} /></ClerkProvider>;
+  return (
+    <ClerkProvider>
+      <Head>
+        <title>Breads</title>
+        <meta name="description" content="🍞" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Toaster position="bottom-center" />
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
